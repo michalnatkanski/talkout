281cb328cf3f988c64b9acd58369ac34f001750c
mnatkanski <m.natkanski0@gmail.com>
2024-07-25 13:37:35 +0200

    Add initial SignUp screen

---
+++ src/views/auth/signup/SignUp.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+export const SignUp = () => {
+  return (
+    <SafeAreaView>
+      <Text>SignUp screen</Text>
+    </SafeAreaView>
+  );
+};
