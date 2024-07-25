c6d0f3b4bcba5b58859263905164fb345e9ca399
mnatkanski <m.natkanski0@gmail.com>
2024-07-25 21:28:47 +0200

    Add initial gradients

--- src/styles/colors.tsx
+++ src/styles/colors.tsx
@@ -5,3 +5,8 @@ export const MAIN_COLORS = {
   ACCENT_1: '#FC77A0',
   ACCENT_2: '#5AE2E2',
 };
+
+export const GRADIENT_COLORS = {
+  PRIMARY: ['#0f0e0e', '#1f203b'],
+  SECONDARY: ['#6271e1', '#55389B', '#A03E82'],
+};
