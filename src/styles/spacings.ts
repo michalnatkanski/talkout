e73bd837d13bfb3ecb67671970beb14abc902539
mnatkanski <m.natkanski0@gmail.com>
2024-07-25 14:44:46 +0200

    Add initial spacings

---
+++ src/styles/spacings.ts
@@ -0,0 +1,11 @@
+import {scaleSize} from './mixins'
+
+export const SCALE_100 = scaleSize(100)
+export const SCALE_24 = scaleSize(24)
+export const SCALE_20 = scaleSize(20)
+export const SCALE_18 = scaleSize(18)
+export const SCALE_16 = scaleSize(16)
+export const SCALE_12 = scaleSize(12)
+export const SCALE_10 = scaleSize(10)
+export const SCALE_8 = scaleSize(8)
+export const SCALE_6 = scaleSize(6)
