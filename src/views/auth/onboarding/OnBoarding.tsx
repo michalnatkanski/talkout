f5000aa30fdaddacf028641c07e6fb4bb57876d4
mnatkanski <m.natkanski0@gmail.com>
2024-07-25 13:37:24 +0200

    Add initial onboarding screen

---
+++ src/views/auth/onboarding/OnBoarding.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+export const OnBoarding = () => {
+  return (
+    <SafeAreaView>
+      <Text>OnBoarding screen</Text>
+    </SafeAreaView>
+  );
+};
