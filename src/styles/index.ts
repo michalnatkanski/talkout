26427d40cab75b65511dfdf602cd18b5a383d961
mnatkanski <m.natkanski0@gmail.com>
2024-07-25 14:48:01 +0200

    Add styles imports

---
+++ src/styles/index.ts
@@ -0,0 +1,6 @@
+import * as colors from './colors'
+import * as consts from './constants'
+import * as mixins from './mixins'
+import * as spacings from './spacings'
+
+export {colors, consts, mixins, spacings}
