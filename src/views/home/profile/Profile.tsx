f98ac857c6374f5bb4f12f22fcb78a6bf373b793
mnatkanski <m.natkanski0@gmail.com>
2024-07-25 13:37:06 +0200

    Add initial Profile screen

---
+++ src/views/home/profile/Profile.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+export const Profile = () => {
+  return (
+    <SafeAreaView>
+      <Text>Profile screen</Text>
+    </SafeAreaView>
+  );
+};
