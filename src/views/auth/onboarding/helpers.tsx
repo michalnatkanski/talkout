535e45792cf579a5d72e5405f460ee9068977e2f
mnatkanski <m.natkanski0@gmail.com>
2024-08-06 14:21:45 +0200

    Update helpers for onboarding screen

--- src/views/auth/onboarding/helpers.tsx
+++ src/views/auth/onboarding/helpers.tsx
@@ -1,23 +1,29 @@
+import {AnimationObject} from 'lottie-react-native';
+
 export interface OnboardingProps {
   id: number;
   title: string;
   subtitle: string;
+  animation: AnimationObject;
 }
 
 export const data: OnboardingProps[] = [
   {
     id: 1,
-    title: 'Welcome in talkout',
-    subtitle: 'lorem ipsum 1',
+    title: 'Talkout',
+    subtitle: 'Anonymous conversations. Heartfelt support.',
+    animation: require('../../../assets/animations/lines.json'),
   },
   {
     id: 2,
-    title: 'Welcome in talkout 2',
-    subtitle: 'lorem ipsum 2',
+    title: 'Find Support',
+    subtitle: 'Receive and give help in a safe space.',
+    animation: require('../../../assets/animations/lines.json'),
   },
   {
     id: 3,
-    title: 'Welcome in talkout 3',
-    subtitle: 'lorem ipsum 3',
+    title: 'Start Talking',
+    subtitle: 'Sharing your problems has never been easier.',
+    animation: require('../../../assets/animations/lines.json'),
   },
 ];
