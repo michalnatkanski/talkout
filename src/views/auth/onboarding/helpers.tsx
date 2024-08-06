57c247eabf8a4838b698e6e5c203a6ab8e3d0467
mnatkanski <m.natkanski0@gmail.com>
2024-08-06 09:30:38 +0200

    Add initial onboarding helpers data

---
+++ src/views/auth/onboarding/helpers.tsx
@@ -0,0 +1,23 @@
+export interface OnboardingProps {
+  id: number;
+  title: string;
+  subtitle: string;
+}
+
+export const data: OnboardingProps[] = [
+  {
+    id: 1,
+    title: 'Welcome in talkout',
+    subtitle: 'lorem ipsum 1',
+  },
+  {
+    id: 2,
+    title: 'Welcome in talkout 2',
+    subtitle: 'lorem ipsum 2',
+  },
+  {
+    id: 3,
+    title: 'Welcome in talkout 3',
+    subtitle: 'lorem ipsum 3',
+  },
+];
